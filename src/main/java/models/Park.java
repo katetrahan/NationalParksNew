04f71b9953d391d1705970963eaa0f63b6735417
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-01-26 11:28:07 -0800

    add Park.java

--- src/main/java/models/Park.java
+++ src/main/java/models/Park.java
@@ -1,7 +1,107 @@
 package models;
 
-/**
- * Created by Guest on 1/26/18.
- */
+import java.util.Objects;
+
+
 public class Park {
+    private String name;
+    private String peak;
+    private String size;
+    private String terrain;
+    private String visits;
+    private String resize;
+    private int id;
+
+    public Park(String name, String peak, String size, String terrain, String visits, String resize) {
+        this.name = name;
+        this.peak = peak;
+        this.size = size;
+        this.terrain = terrain;
+        this.visits = visits;
+        this.resize = resize;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPeak() {
+        return peak;
+    }
+
+    public void setPeak(String peak) {
+        this.peak = peak;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getTerrain() {
+        return terrain;
+    }
+
+    public void setTerrain(String terrain) {
+        this.terrain = terrain;
+    }
+
+    public String getVisits() {
+        return visits;
+    }
+
+    public void setVisits(String visits) {
+        this.visits = visits;
+    }
+
+    public String getResize() {
+        return resize;
+    }
+
+    public void setResize(String resize) {
+        this.resize = resize;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Park park = (Park) o;
+
+        if (id != park.id) return false;
+        if (!name.equals(park.name)) return false;
+        if (peak != null ? !peak.equals(park.peak) : park.peak != null) return false;
+        if (size != null ? !size.equals(park.size) : park.size != null) return false;
+        if (terrain != null ? !terrain.equals(park.terrain) : park.terrain != null) return false;
+        if (visits != null ? !visits.equals(park.visits) : park.visits != null) return false;
+        return resize != null ? resize.equals(park.resize) : park.resize == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + (peak != null ? peak.hashCode() : 0);
+        result = 31 * result + (size != null ? size.hashCode() : 0);
+        result = 31 * result + (terrain != null ? terrain.hashCode() : 0);
+        result = 31 * result + (visits != null ? visits.hashCode() : 0);
+        result = 31 * result + (resize != null ? resize.hashCode() : 0);
+        result = 31 * result + id;
+        return result;
+    }
 }
