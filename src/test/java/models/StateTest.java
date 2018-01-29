0e7326f3a6b6931fe37317c1e27db589559e3d2f
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-01-28 19:11:45 -0800

    add tests to State Test (forgot originally)

--- src/test/java/models/StateTest.java
+++ src/test/java/models/StateTest.java
@@ -1,10 +1,75 @@
 package models;
 
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Created by Guest on 1/26/18.
- */
+import static org.junit.Assert.*;
+
+
 public class StateTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    public State setupState() {
+        return new State("Oregon", "1", "4.093 million");
+    }
+
+
+    @Test
+    public void getNameReturnsCorrectly() throws Exception {
+        State newState = setupState();
+        assertEquals("Oregon", newState.getName());
+    }
+
+    @Test
+    public void getNumberOfParksReturnsCorrectly() throws Exception {
+        State newState = setupState();
+        assertEquals("1", newState.getNumberOfParks());
+    }
+
+    @Test
+    public void getPopultaionReturnsCorrectly() throws Exception {
+        State newState = setupState();
+        assertEquals("4.093 million", newState.getPopulation());
+    }
+
+    @Test
+    public void setNameSetsCorrectly() throws Exception {
+        State newState = setupState();
+        newState.setName("Ellen Degeneras");
+        assertNotEquals("Oprah", newState.getName());
+    }
+
+    @Test
+    public void setNumberOfParksReturnsCorrectly() throws Exception {
+        State newState = setupState();
+        newState.setNumberOfParks("100");
+        assertNotEquals("1", newState.getNumberOfParks());
+    }
+
+    @Test
+    public void setPopulationReturnCorrectly() throws Exception {
+        State newState = setupState();
+        newState.setPopulation("6 million");
+        assertNotEquals("4.093 million", newState.getPopulation());
+    }
+
+
+
+
+
+
+
+
 
 }
